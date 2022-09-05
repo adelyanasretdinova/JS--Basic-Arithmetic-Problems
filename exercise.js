5b9a26b3ced4a2eb6bfc0e5a9fd29cc4e648e99b
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2022-09-05 10:54:54 +0200

    Corrected

--- exercise.js
+++ exercise.js
@@ -22,15 +22,16 @@ let result = (number1/number2)*100;
 let percent = '%';
 
 // Result: percent of 17 out of 30:
-console.log(result, percent);
+console.log("Q3:",result, percent);
 
 
 // * Exercise 4
 let sideSize = 4.75;
 let cm = "cm";
+const numberOfSquareSides = 4;
 
 // Result: Perimeter of square:
-console.log(sideSize*4, cm);
+console.log(`Perimeter of square ${(sideSize * numberOfSquareSides)} cm`);
 
 
 // * Exercise 5
@@ -39,13 +40,13 @@ let side2 = 6;
 let side3 = 7;
 
 // Result: Perimeter of a triangle:
-console.log(side1 + side2 + side3, cm);
+console.log(`Perimeter of triangle: ${(side1 + side2 + side3)} cm`);
 
 
 // * Exercise 6
 
 // Result: The area of a square:
-console.log(side1**2, cm);
+console.log(`Area of square: ${(side1 ** 2)} cm`);
 
 
 // * Exercise 7
@@ -54,26 +55,26 @@ let sideB = 4;
 let sideC = 5;
 
 // Result: The area of an right-angled triangle:
-console.log(sideA*sideB*0.5, cm);
+console.log(`Area of right-angled triangle: ${(sideA * sideB * 0.5)} cm`);
 
 
 // * Exercise 8
-let CubeSide = 9;
+let cubeSide = 9;
 
 // Result: The area of an right-angled triangle:
-console.log(CubeSide** 3, cm);
+console.log(`Volume of cube: ${(cubeSide ** 3)} cm`);
 
 // * Exercise 9
-let Euro = '€';
+let euro = '€';
 let bill1 = 22.35;
 let bill1Tip = 0.1;
-let bill1Total = bill1*(1+bill1Tip) + Euro;
+let bill1Total = bill1 * (1 + bill1Tip) + euro;
 let bill2 = 26.67;
 let bill2Tip = 0.15;
-let bill2Total = bill2*(1+bill2Tip) + Euro;
+let bill2Total = bill2 * (1 + bill2Tip) + euro;
 let bill3 = 35.92;
 let bill3Tip = 0.2;
-let bill3Total = bill3*(1+bill3Tip) + Euro;
+let bill3Total = bill3 * (1 + bill3Tip) + euro;
 
 // Result: Total value of each bill with Tips:
 console.log(bill1Total, bill2Total, bill3Total);
@@ -81,18 +82,17 @@ console.log(bill1Total, bill2Total, bill3Total);
 
 // * Exercise 10
 let workingHours = [8, 6, 5, 9, 8, 2, 1, 8.5, 7, 4];
-let Hours = "hours";
+let hours = "hours";
 let average = workingHours.reduce((accumulator, element) => accumulator + element, 0) / workingHours.length;
-//let average = workingHours.reduce(function (acc, el) {return acc + el}, 0)
 // Result: Average hours worked per day:
-console.log(average, Hours);
+console.log(average, hours);
 
 // * Exercise 11
 let examResult = [75, 70, 85, 90, 100];
 let averageResult = 85;
 // let TotalResultSum=averageResult*(examResult.length+1);
-let LastTestResult = averageResult * (examResult.length + 1)-examResult.reduce((accumulator, element) => accumulator + element, 0);
-console.log("Score in the sixth test:", LastTestResult);
+let lastTestResult = averageResult * (examResult.length + 1)-examResult.reduce((accumulator, element) => accumulator + element, 0);
+console.log("Score in the sixth test:", lastTestResult);
 
 
 // * Exercise 12
@@ -103,4 +103,4 @@ console.log("Score in the sixth test:", LastTestResult);
  let currentArrayLength = 8;
  let currentSum = currentAv * currentArrayLength;
  let minTestResult = totalSum - currentSum;
- console.log("James needs a minimum of", minTestResult, "% to get an 80% average");
+ console.log(`James needs a minimum of ${minTestResult}% to get an 80% average`);
